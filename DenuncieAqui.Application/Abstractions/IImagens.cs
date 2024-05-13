0445f2083690e553b08087ba79c36da0953aa860
IGOR OLIVEIRA <igor.oliveira53@fatec.sp.gov.br>
2024-05-12 23:34:50 -0300

    Create Abstractions.

---
+++ DenuncieAqui.Application/Abstractions/IImagens.cs
@@ -0,0 +1,18 @@
+﻿using DenuncieAqui.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DenuncieAqui.Application.Abstractions
+{
+    public interface IImagens
+    {
+        Task<IEnumerable<Images>> GetImagesAsync();
+
+        Task <Images> AddImagesAsync(Images images);
+
+        Task DeleteImagesAsync(int id);
+    }
+}
---
+++ DenuncieAqui.Application/Abstractions/ILikes.cs
@@ -0,0 +1,18 @@
+﻿using DenuncieAqui.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DenuncieAqui.Application.Abstractions
+{
+    public interface ILikes
+    {
+        Task<IEnumerable<Likes>> GetLikesAsync();
+
+        Task <Likes> AddLikesAsync(Likes likes);
+
+        Task RemoveLikesAsync(int id);
+    }
+}
