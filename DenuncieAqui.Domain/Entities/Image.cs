e628fa7883a2fa56c90965e1c6c9884b23feedbb
IGOR OLIVEIRA <igor.oliveira53@fatec.sp.gov.br>
2024-08-19 00:01:28 -0300

    Image Testing - Pré Finish Version

--- DenuncieAqui.Domain/Entities/Image.cs
+++ DenuncieAqui.Domain/Entities/Image.cs
@@ -11,16 +11,28 @@ public class Image : Entity
 
     public string ImageUrl { get; set; } = string.Empty;
 
+    public byte[]? ConteudoArquivo { get; set; }
+
     public DateTime ImageDate { get; set; }
 
     public virtual Guid ReportId { get; set; }
 
     public virtual Report? Report { get; set; }
 
-    public Image(string imageUrl, DateTime imageDate, Guid reportId) : base()
+    //public Image(string imageUrl, byte[]? conteudoArquivo, DateTime imageDate, Guid reportId) : base()
+    //{
+    //    ImageUrl = imageUrl;
+    //    ConteudoArquivo = conteudoArquivo;
+    //    ImageDate = imageDate;
+    //    ReportId = reportId;
+
+    //}
+
+    public Image(string imageUrl, byte[]? conteudoArquivo, DateTime imageDate, Guid reportId) : base()
     {
         ImageUrl = imageUrl;
         ImageDate = imageDate;
         ReportId = reportId;
+        ConteudoArquivo = conteudoArquivo;
     }
 }
