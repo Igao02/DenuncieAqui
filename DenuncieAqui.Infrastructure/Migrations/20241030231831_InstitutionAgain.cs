f3c413d2de5b2c277f802b4df127458f43bf25d8
IGOR OLIVEIRA <igor.oliveira53@fatec.sp.gov.br>
2024-10-30 20:24:11 -0300

    Migration Institution

---
+++ DenuncieAqui.Infrastructure/Migrations/20241030231831_InstitutionAgain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DenuncieAqui.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InstitutionAgain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreationDate",
+                table: "Institutions",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Complement",
+                table: "Institutions",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Complement",
+                table: "Institutions");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreationDate",
+                table: "Institutions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
